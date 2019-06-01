4d8b60813b2067462852060c22b0753949e45251
kursivee <kursivee.dev@gmail.com>
2019-06-01 10:41:33 -0400

    Initial androidx commit

---
+++ app/src/main/java/com/kursivee/authentication/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.kursivee.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kursivee.authentication.ui.login.LoginFragment
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, LoginFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
